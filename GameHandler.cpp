e9b7c158967e8a28e8c5e159013b4c0108988111
ArturHamerski <arturhamerski98@gmail.com>
2022-06-29 09:46:58 +0200

    sroda

--- GameHandler.cpp
+++ GameHandler.cpp
@@ -1,1 +1,97 @@
 #include "GameHandler.h"
+#include "Player.h"
+#include "Menu.h"
+#include "windows.h"
+
+
+
+void GameHandler::getTurnLimit()
+{
+	do {
+
+		int limit;
+		std::cout << "\nLimit tur: ";
+		std::cin >> limit;
+		turnLimit = limit;
+		if ((turnLimit <= 50) && (turnLimit >= 5));
+		std::cout << "\nLimit tur musi byc wiekszy od 5 i mniejszy od 50\n";
+	} while (!((turnLimit <= 50) && (turnLimit >= 5)));
+
+}
+
+int GameHandler::getBoardSize()
+{
+	int x;
+	do {
+		std::cout << "\nWpradz rozmiar planszy: ";
+		std::cin >> x;
+		std::cout << std::endl;
+		BoardSize = x;
+		if (!((x >= 5) && (x <= 10)))
+			std::cout << "Wprowadz rormiar od 5 do 10";
+	} while (!((x >= 5) && (x <= 10)));
+	setBoardSize(x);
+	return BoardSize;
+}
+
+void GameHandler::setBoardSize(int bs)
+{
+	BoardSize = bs;
+}
+
+
+void GameHandler::displayShotResult()
+{
+}
+void GameHandler::pVp()
+{
+	Player player1(BoardSize);
+	player1.PlaceShips();
+	std::cout << "\n";
+	Sleep(2000);
+	system("cls");
+	Player player2(BoardSize);
+	player2.PlaceShips();
+	Sleep(2000);
+	system("cls");
+	player1.PrintPlayerInfo();
+	Sleep(2000);
+	system("cls");
+	player2.PrintPlayerInfo();
+	Sleep(2000);
+	system("cls");
+
+
+	while (turnLimit > 0) {
+		if (turnLimit % 2 == 0)
+		{
+			std::cout << "Zostalo tur: " << turnLimit << "\n";
+			std::cout << "Podaj x miejsca strzalu: \n";
+
+			std::cout << "Podaj y miejsca strzalu: \n";
+
+			player1.TakeAShot(player2);
+			//std::cout << "strzela p1 ";
+			turnLimit--;
+
+			Sleep(3000);
+			system("cls");
+		}
+		else
+		{
+			std::cout << "Zostalo tur: " << turnLimit << "\n";
+			std::cout << "Podaj x miejsca strzalu: \n";
+
+			std::cout << "Podaj y miejsca strzalu: \n";
+
+			player2.TakeAShot(player2);
+			//std::cout << "strzela p2";
+			turnLimit--;
+			Sleep(3000);
+			system("cls");
+		}
+	}
+	std::cout << "Koniec gry";
+}
+
+
--- GameHandler.h
+++ GameHandler.h
@@ -1,19 +1,28 @@
-#pragma once
+ï»¿#pragma once
 class GameHandler
 {
 
-	/*Zadanie: Trzeba stworzyæ klasê GameHandler w która :
-	*Tworzy dwóch graczy
-	* Dwaj gracze Tworz¹ swoje statki
-	* Gracze wykonuj¹ ruch na zmianê
-	 - wyœwietla sie informacja, którego gracza jest tura
-	 -wyswietlaja sie 2 plansze
-	 -prosi gracza o podanie koordynatow(tylko cout nie implementacja)
-	 - informujemy czy trafienie czy pudlo(tylko cout nie implementacja)
-	 -czekamy 3sekundy, czyscimy konsole, zmiana tury na gracza numer 2
-	 -zrobiæ Level4
-	*
-	* */
-	
-};
+	int turnLimit;
+protected:
+	int BoardSize;
+	void setBoardSize(int bs);
 
+public:
+	void pVp();
+	int getBoardSize();
+	void displayShotResult();
+	void getTurnLimit();
+	/*Zadanie: Trzeba stworzyï¿½ klasï¿½ GameHandler w ktï¿½ra :
++*Tworzy dwï¿½ch graczy
++* Dwaj gracze Tworzï¿½ swoje statki
+* Gracze wykonujï¿½ ruch na zmianï¿½
+ *- wyï¿½wietla sie informacja, ktï¿½rego gracza jest tura
+* -wyswietlaja sie 2 plansze
+ -prosi gracza o podanie koordynatow(tylko cout nie implementacja)
+ - informujemy czy trafienie czy pudlo(tylko cout nie implementacja)
++ -czekamy 3sekundy, czyscimy konsole, zmiana tury na gracza numer 2
++ -zrobiï¿½ Level4
+
+* */
+
+};
