1ae37e8b985822011e0182b072ae9ada9d1ddbed
KrystianKiznar <krystian.kiznar@gmail.com>
2022-06-30 10:25:53 +0200

    czw 10.25

--- Menu.cpp
+++ Menu.cpp
@@ -1,28 +1,8 @@
 #include "Menu.h"
 #include <iostream>
 
-void Menu::printMenu()
-{
-}
-
-=======
 
 void  Menu::printMenu()
 {
-    int x;
-    std::cout << "Wpraowaaaaaaaaaaadz rozmiar planszy: \n";
-    std::cin >> x;
-    std::cout << std::endl;
-    setBoardSize(x);
-
 }
 
-int Menu::getBoardSize() {
-
-    return BoardSize;
-}
-
-
-Menu::Menu() {
-    BoardSize = 5;
-}
